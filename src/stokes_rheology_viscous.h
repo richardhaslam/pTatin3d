9f4c70267dac071720be8b21871b20591199692c
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2012-02-14 19:47:47 +0000

    Added basic iso viscous rheology
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@2418 26174531-3d5a-4d31-aba0-7a6658d032cb

---
+++ src/stokes_rheology_viscous.h
@@ -0,0 +1,7 @@
+
+#ifndef __ptatin_stokes_rheology_viscous_h__
+#define __ptatin_stokes_rheology_viscous_h__
+
+PetscErrorCode EvaluateRheologyNonlinearitiesMarkers_Viscous(pTatinCtx user,DM dau,PetscScalar u[],DM dap,PetscScalar p[]);
+
+#endif
