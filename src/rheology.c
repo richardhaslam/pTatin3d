db4d987d1ed31e5662007087f9f9ff3f89cd1b17
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2012-02-14 18:52:53 +0000

    Added rheology context. Note I added a new variable: 		PetscInt     nphases_active; This is initialized to 0, and model setups SHOULD define the max number of phases.
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@2412 26174531-3d5a-4d31-aba0-7a6658d032cb

---
+++ src/rheology.c
@@ -0,0 +1,160 @@
+
+#include "petsc.h"
+
+#include "private/ptatin3d_ctx_impl.h"
+#include "ptatin3d.h"
+
+#include "MPntStd_def.h"
+#include "MPntPStokes_def.h"
+#include "swarm_fields.h"
+
+#include "rheology.h"
+
+
+#undef __FUNCT__
+#define __FUNCT__ "RheologyConstantsInitialise"
+PetscErrorCode RheologyConstantsInitialise(RheologyConstants *R)
+{	
+  PetscInt p;
+  PetscBool flg;
+  PetscScalar vis;
+  PetscErrorCode ierr; 
+  
+	PetscFunctionBegin;
+	
+	R->nphases_active = 0;
+	
+  /* Define defaults for the viscosity cut-offs */
+  R->apply_viscosity_cutoff_global = PETSC_FALSE;
+  R->eta_lower_cutoff_global = 1.0e-100;
+  R->eta_upper_cutoff_global = 1.0e+100;
+  
+  
+  flg = PETSC_FALSE;
+  ierr = PetscOptionsGetReal(PETSC_NULL,"-eta_lower_cutoff_global",&vis,&flg);CHKERRQ(ierr);
+  
+  if (flg == PETSC_TRUE) { 
+    R->apply_viscosity_cutoff_global = PETSC_TRUE;
+    R->eta_lower_cutoff_global       = vis;
+  }
+  
+	flg = PETSC_FALSE;
+  ierr = PetscOptionsGetReal(PETSC_NULL,"-eta_upper_cutoff_global",&vis,&flg);CHKERRQ(ierr);  
+  
+  if (flg == PETSC_TRUE) { 
+    R->apply_viscosity_cutoff_global = PETSC_TRUE;
+    R->eta_upper_cutoff_global       = vis;
+  }
+  
+  PetscPrintf(PETSC_COMM_WORLD," global viscosity cut-off, min= %1.6e, max = %1.6e  \n", R->eta_lower_cutoff_global, R->eta_upper_cutoff_global );
+  
+  
+  /* phase cutoff is equal to global cutoff and for the moment there is no options to enforce it 
+   I don't think it belongs here ... maybe to the model definition */ 
+  R->apply_viscosity_cutoff        = PETSC_FALSE;
+  
+  for (p=0; p<MAX_PHASE; p++) {
+    R->eta_lower_cutoff[p] = R->eta_lower_cutoff_global;
+    R->eta_upper_cutoff[p] = R->eta_upper_cutoff_global;
+  }
+  
+  /* Define defaults for the parameters for each rheology */
+	PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "pTatin_EvaluateRheologyNonlinearitiesMarkers"
+PetscErrorCode pTatin_EvaluateRheologyNonlinearitiesMarkers(pTatinCtx user,DM dau,PetscScalar u[],DM dap,PetscScalar p[])
+{	
+  RheologyConstants *rheo;
+	int               npoints;
+	DataField         PField_std;
+	DataField         PField_stokes;
+  MPntStd           *mp_std;
+	MPntPStokes       *mp_stokes;
+	PetscErrorCode    ierr;
+  static int        been_here=0;
+	
+	
+	PetscFunctionBegin;
+  rheo = &user->rheology_constants;
+	switch (rheo->rheology_type) {
+
+		case RHEOLOGY_VISCOUS:
+			if (been_here==0) {
+				PetscPrintf(PETSC_COMM_WORLD,"*** WARNING: Rheology update for RHEOLOGY_VISCOUS using markers is under development ***\n");
+				been_here = 1;
+			}
+			/* update on markers */
+			//ierr = EvaluateRheologyNonlinearitiesMarkers_Viscous(user,dau,u,dap,p);CHKERRQ(ierr);
+			
+			
+			break;
+			
+		case RHEOLOGY_VISCO_PLASTIC:
+			if (been_here==0) {
+				PetscPrintf(PETSC_COMM_WORLD,"*** WARNING: Rheology update for RHEOLOGY_VISCO_PLASTIC using markers is under development ***\n");
+				been_here = 1;
+			}
+			/* update on markers */
+			SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Rheology update for RHEOLOGY_VISCO_PLASTIC using markers not defined");
+			//ierr = EvaluateRheologyNonlinearitiesMarkers_ViscoPlastic(user,dau,u,dap,p);CHKERRQ(ierr);
+			
+			break;
+			
+		case RHEOLOGY_VISCO_PLASTIC_STRAIN_WEAKENING:
+			if (been_here==0) {
+				PetscPrintf(PETSC_COMM_WORLD,"*** WARNING: Rheology update for RHEOLOGY_VISCO_PLASTIC_STRAIN_WEAKENING using markers is under development ***\n");
+				been_here = 1;
+			}
+			/* update on markers */
+			SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Rheology update for RHEOLOGY_VISCO_PLASTIC_STRAIN_WEAKENING using markers not defined");
+			//ierr = EvaluateRheologyNonlinearitiesMarkers_ViscoPlasticStrainWeakening(user,dau,u,dap,p);CHKERRQ(ierr);
+			break;
+			
+		case RHEOLOGY_VISCO_ELASTIC_PLASTIC:
+			SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Rheology update for RHEOLOGY_VISCO_ELASTIC_PLASTIC using markers not defined");
+			break;
+			
+		default:
+			
+			break;
+	}
+
+  /* Marker -> quadrature point projection */
+	DataBucketGetDataFieldByName(user->materialpoint_db, MPntStd_classname     , &PField_std);
+	DataBucketGetDataFieldByName(user->materialpoint_db, MPntPStokes_classname , &PField_stokes);
+	
+	DataBucketGetSizes(user->materialpoint_db,&npoints,PETSC_NULL,PETSC_NULL);
+	mp_std    = PField_std->data; /* should write a function to do this */
+	mp_stokes = PField_stokes->data; /* should write a function to do this */
+	
+	switch (user->coefficient_projection_type) {
+
+		case 0:			/* Perform P0 projection over Q2 element directly onyo uadrature points */
+			SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"P0 marker->quadrature projection not supported");
+			break;
+			
+		case 1:			/* Perform Q1 projection over Q2 element and interpolate back to quadrature points */
+			SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Q1 marker->quadrature projection not supported");
+			break;
+			
+		case 2: 			/* Perform Q2 projection and interpolate back to quadrature points */
+			SETERRQ(PETSC_COMM_WORLD,PETSC_ERR_SUP,"Q2 marker->quadrature projection not supported");
+			break;
+	}
+	
+  PetscFunctionReturn(0);
+}
+
+#undef __FUNCT__
+#define __FUNCT__ "pTatin_EvaluateRheologyNonlinearities"
+PetscErrorCode pTatin_EvaluateRheologyNonlinearities(pTatinCtx user,DM dau,PetscScalar u[],DM dap,PetscScalar p[])
+{
+	PetscErrorCode ierr;
+	PetscFunctionBegin;
+	
+	ierr = pTatin_EvaluateRheologyNonlinearitiesMarkers(user,dau,u,dap,p);CHKERRQ(ierr);
+	
+	PetscFunctionReturn(0);
+}
---
+++ src/rheology.h
@@ -0,0 +1,52 @@
+
+#ifndef __ptatin_rheology_h__
+#define __ptatin_rheology_h__
+
+#include "ptatin3d.h"
+
+typedef enum { 
+	RHEOLOGY_VISCOUS=0, 
+	RHEOLOGY_VISCO_PLASTIC, 
+	RHEOLOGY_VISCO_PLASTIC_STRAIN_WEAKENING, 
+	RHEOLOGY_VISCO_ELASTIC_PLASTIC 
+} RheologyType;
+
+typedef struct _p_RheologyConstants RheologyConstants;
+
+#define MAX_PHASE 100
+
+struct _p_RheologyConstants {
+  PetscBool apply_viscosity_cutoff_global;
+  PetscReal eta_lower_cutoff_global, eta_upper_cutoff_global;
+  PetscBool apply_viscosity_cutoff;
+  PetscReal eta_lower_cutoff[MAX_PHASE], eta_upper_cutoff[MAX_PHASE];
+  
+  /* rheology; "constant" ==>> const (short name) */
+  PetscReal const_eta0[MAX_PHASE];
+  PetscReal const_rho0[MAX_PHASE];
+	/* not in used in the marker version, elastic const,would require storing stress on marker... oh la la */
+  PetscReal const_shearmod[MAX_PHASE];    
+  /* rheology; "von mises" ==>> mises (short name) */
+  PetscReal mises_tau_yield[MAX_PHASE]; /* would be nice to refactor it to DP_Co */ 
+  PetscReal dp_pressure_dependance[MAX_PHASE]; /* would be nice to refactor it to DP_phi */
+  PetscReal tens_cutoff[MAX_PHASE]; /* would be nice to refactor it to DP_tens_cutoff */
+  PetscReal Hst_cutoff [MAX_PHASE]; /* would be nice to refactor it to DP_Hst_cutoff */
+	
+  PetscReal gamma_soft[MAX_PHASE]; /* not in used in the marker version */ 
+  PetscReal mises_tau_yield_inf[MAX_PHASE]; /* not in used in the marker version */
+	
+  /* rheology; "strain_weakening" ==> soft (short name) */
+  PetscReal soft_min_strain_cutoff [MAX_PHASE];
+  PetscReal soft_max_strain_cutoff [MAX_PHASE];
+  PetscReal soft_Co_inf [MAX_PHASE];
+  PetscReal soft_phi_inf [MAX_PHASE];
+	
+	PetscInt     nphases_active;
+	RheologyType rheology_type;
+};
+
+
+PetscErrorCode RheologyConstantsInitialise(RheologyConstants *R);
+PetscErrorCode pTatin_EvaluateRheologyNonlinearities(pTatinCtx user,DM dau,PetscScalar u[],DM dap,PetscScalar p[]);
+
+#endif
