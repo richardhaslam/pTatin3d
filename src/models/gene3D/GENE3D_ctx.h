e90efbf363c84f53649fd4cea4b2931036c47a03
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2012-02-15 15:25:40 +0000

    Couple of c bugs fixed
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@2430 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/models/gene3D/GENE3D_ctx.h
+++ src/models/gene3D/GENE3D_ctx.h
@@ -5,7 +5,8 @@
 
 typedef enum { GENEBC_FreeSlip=0, GENEBC_NoSlip, GENEBC_FreeSlipFreeSurface, GENEBC_NoSlipFreeSurface } GENE3DBC;
 typedef enum { GENE_LayeredCake=0, GENE_ExtrudeFromMap, GENE_ReadFromCAD} GENE3DINIGEOM;
-const LAYER_MAX = 100; 
+const PetscInt LAYER_MAX = 100; 
+
 typedef struct {
 	PetscInt  nmaterials;
 	PetscReal Lx,Ly,Lz;
@@ -14,6 +15,7 @@ typedef struct {
 	GENE3DBC  boundary_conditon_type; /* [ 0 free slip | 1 no slip | 2 free surface + free slip | 3 free surface + no slip ] */
 	GENE3DINIGEOM  initial_geom;
 } ModelGENE3DCtx;
+
 PetscErrorCode ModelSetMarkerIndexLayeredCake_GENE3D(pTatinCtx c,void *ctx);
 PetscErrorCode ModelSetMarkerIndexFromMap_GENE3D(pTatinCtx c,void *ctx);
 PetscErrorCode ModelSetInitialStokesVariableOnMarker_GENE3D(pTatinCtx c,void *ctx);
