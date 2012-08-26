91f0c0aff229d93881d547aa45755e2e1ae20a4a
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2012-08-26 13:26:58 +0000

    updated to use material constants
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@3208 26174531-3d5a-4d31-aba0-7a6658d032cb

---
+++ src/models/gene3d_nueve/model_gene3d_ctx.h
@@ -0,0 +1,21 @@
+
+
+#ifndef __ptatinmodel_GENE3DNUEVE_ctx_h__
+#define __ptatinmodel_GENE3DNUEVE_ctx_h__
+
+typedef enum { GENEBC_FreeSlip=0, GENEBC_NoSlip, GENEBC_FreeSlipFreeSurface, GENEBC_NoSlipFreeSurface } GENE3DBC;
+typedef enum { GENE_LayeredCake=0, GENE_ExtrudeFromMap, GENE_ReadFromCAD} GENE3DINIGEOM;
+
+typedef struct {
+	PetscInt  nmaterials;
+	PetscReal Lx,Ly,Lz;
+	PetscReal Ox,Oy,Oz;
+	GENE3DBC  boundary_conditon_type; /* [ 0 free slip | 1 no slip | 2 free surface + free slip | 3 free surface + no slip ] */
+	GENE3DINIGEOM  initial_geom;
+} ModelGene3DNueveCtx;
+
+PetscErrorCode ModelSetMarkerIndexLayeredCake_Gene3DNueve(pTatinCtx c,void *ctx);
+PetscErrorCode ModelSetMarkerIndexFromMap_Gene3DNueve(pTatinCtx c,void *ctx);
+PetscErrorCode ModelSetInitialStokesVariableOnMarker_Gene3DNueve(pTatinCtx c,void *ctx);
+		
+#endif
