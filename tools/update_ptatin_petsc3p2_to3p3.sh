205238985426269f347ab5e64b25b1d1afd2e389
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-02-01 16:52:20 +0000

    added couple of extra conversion for compat with petsc 3.3
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@3802 26174531-3d5a-4d31-aba0-7a6658d032cb

--- tools/update_ptatin_petsc3p2_to3p3.sh
+++ tools/update_ptatin_petsc3p2_to3p3.sh
@@ -11,6 +11,13 @@ perl -p -i -e 's/DMGetInjection/DMCreateInjection/g' *.c
 perl -p -i -e 's/DMGetInterpolationScale/DMCreateInterpolationScale/g' *.c
 perl -p -i -e 's/PetscTypeCompare/PetscObjectTypeCompare/g' *.c
 
-cd test_option_files
-perl -p -i -e 's/schur\_factorization\_type/schur\_fact\_type/g' *.opts 
+perl -p -i -e 's/ierr\s\=\sPCMGSetGalerkin\(pc\_i,PETSC\_FALSE\);CHKERRQ\(ierr\)\;/ierr = PCMGSetGalerkin(pc\_i,PETSC\_FALSE);CHKERRQ(ierr);\n\t\tierr = PCSetDM(pc_i,PETSC\_NULL);CHKERRQ(ierr);/g' *.c
 
+perl -p -i -e 's/ierr\s=\sMatSetType\(\sA,\sMATAIJ\s\)\;CHKERRQ\(ierr\)\;/ierr = MatSetType( A, MATAIJ );CHKERRQ(ierr);\n\tierr = MatSetUp(A);CHKERRQ(ierr);/g' data_exchanger.c
+
+perl -p -i -e 's/ierr\s=\sMatSetBlockSize\(B,3\)\;CHKERRQ\(ierr\)\;/\/*ierr = MatSetBlockSize(B,3);CHKERRQ(ierr);*\//g' stokes_operators.c
+
+cd test_option_files
+perl -p -i -e 's/da\_mat\_type/dm\_mat\_type/g' *.opts 
+perl -p -i -e 's/schur\_factorization\_type/schur\_fact\_type/g' *.opts 
+perl -p -i -e 's/chebychev/chebyshev/g' *.opts
