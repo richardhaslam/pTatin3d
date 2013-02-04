ce0d5f07250b5f4b275b3aca8303584f567a2720
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-02-04 09:08:19 +0000

    add prototypes
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@3817 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/quadrature.h
+++ src/quadrature.h
@@ -38,5 +38,8 @@ PetscErrorCode QuadratureCreate(Quadrature *quadrature);
 PetscErrorCode QuadratureDestroy(Quadrature *quadrature);
 PetscErrorCode QuadratureView(Quadrature q);
 
+void QuadratureCreateGauss_2pnt_3D(PetscInt *ngp,PetscReal **_q_coor,PetscReal **_q_weight);
+void QuadratureCreateGauss_3pnt_3D(PetscInt *ngp,PetscReal **_q_coor,PetscReal **_q_weight);
+
 #endif
 
