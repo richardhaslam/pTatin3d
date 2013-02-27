8978b5ee933f9786a854ab8a1ee6fb52aaa21995
svnuser <svnuser@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-02-27 11:36:01 +0000

    Angle metric and Faceratio metric clearer
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@3920 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/mesh_quality_metrics.c
+++ src/mesh_quality_metrics.c
@@ -86,10 +86,10 @@ typedef enum {
 
 /* protoypes for helpers */
 void get_node_coordinate(double el_coords[],int index,double pos[]);
-void get_face_coordinates(double el_coords[],Q2Face face,double coords[]);
-void get_corner_vectors(double el_coords[],int corner,double vectors[]);
-double compute_quadrilateral_area_(double coners[]);
-void compute_cossin(double vectors[], double cossin[]);
+void get_face_coordinates(double el_coords[],Q2Face face,double coords[][3]);
+void get_vertex_vectors(double el_coords[],int vertex,double vectors[][3]);
+double compute_quadrilateral_area_(double vertex[][3]);
+void compute_cossin(double vectors[][3], double cossin[]);
 double compute_distance3(double posA[],double posB[]);
 double compute_norm3(double a[]);
 double compute_dot_product3(double a[],double b[]);
@@ -118,7 +118,7 @@ void get_node_coordinate(double el_coords[],int index,double pos[])
  F_SOUTH: 0,  2,  20, 18
  F_NORTH: 6,  24, 26, 8
  */
-void get_face_coordinates(double el_coords[],Q2Face face,double coords[])
+void get_face_coordinates(double el_coords[],Q2Face face,double coords[][3])
 {
 	int nodes[4], i;
 	
@@ -150,9 +150,9 @@ void get_face_coordinates(double el_coords[],Q2Face face,double coords[])
 	}
 	
 	for(i = 0; i < 4; i++){
-		coords[i*3 + 0] = el_coords[3*nodes[i] + 0];
-		coords[i*3 + 1] = el_coords[3*nodes[i] + 1];
-		coords[i*3 + 2] = el_coords[3*nodes[i] + 2];
+		coords[i][0] = el_coords[3*nodes[i] + 0];
+		coords[i][1] = el_coords[3*nodes[i] + 1];
+		coords[i][2] = el_coords[3*nodes[i] + 2];
 	}
 }
 
@@ -168,11 +168,11 @@ void get_face_coordinates(double el_coords[],Q2Face face,double coords[])
  C24: 24-6, 24-26, 24-18
  C26: 26-24, 26-8, 26-20
 */
-void get_corner_vectors(double el_coords[],int corner,double vectors[])
+void get_vertex_vectors(double el_coords[],int vertex,double vectors[][3])
 {
 	int nodes[4], i;
 	
-	switch (corner) {
+	switch (vertex) {
 			
 		case 0:
 			nodes[0] = 0;   nodes[1] = 18;  nodes[2] = 2;   nodes[3] = 6;
@@ -207,9 +207,9 @@ void get_corner_vectors(double el_coords[],int corner,double vectors[])
 			break;					
 	}
 	for(i = 0; i < 3; i++){
-		vectors[i*3 + 0] = el_coords[3*nodes[i+1] + 0] - el_coords[3*nodes[0] + 0];
-		vectors[i*3 + 1] = el_coords[3*nodes[i+1] + 1] - el_coords[3*nodes[0] + 1];
-		vectors[i*3 + 2] = el_coords[3*nodes[i+1] + 2] - el_coords[3*nodes[0] + 2];
+		vectors[i][0] = el_coords[3*nodes[i+1] + 0] - el_coords[3*nodes[0] + 0];
+		vectors[i][1] = el_coords[3*nodes[i+1] + 1] - el_coords[3*nodes[0] + 1];
+		vectors[i][2] = el_coords[3*nodes[i+1] + 2] - el_coords[3*nodes[0] + 2];
 	}  
 }
 
@@ -223,27 +223,21 @@ void compute_cross_product3(double a[],double b[],double crossprod[])
 	crossprod[2] =    a[0]*b[1] - a[1]*b[0];
 }
 
-double compute_quadrilateral_area_(double coners[])
+double compute_quadrilateral_area_(double vertex[][3])
 {
-	int i, c1, c2, idx;
-	double crossprod[12], scalarprod[4], normcrossprod[4];
+	int i, c1, c2;
+	double crossprods[4][3], scalarprod[4], normcrossprod[4], a[3], b[3];
 	
 	for(i = 0; i<4; i++){
 		c1 = (i+1)%4;
 		c2 = (i+3)%4;
-		idx = 0;
-		crossprod[3*i+idx] = (coners[c1*3+(1+idx)%3] - coners[i*3+(1+idx)%3])*(coners[c2*3+(2+idx)%3] - coners[i*3+(2+idx)%3])
-		- (coners[c2*3+(1+idx)%3] - coners[i*3+(1+idx)%3])*(coners[c1*3+(2+idx)%3] - coners[i*3+(1+idx)%3]); ++idx;
-		crossprod[3*i+idx] = (coners[c1*3+(1+idx)%3] - coners[i*3+(1+idx)%3])*(coners[c2*3+(2+idx)%3] - coners[i*3+(2+idx)%3])
-		- (coners[c2*3+(1+idx)%3] - coners[i*3+(1+idx)%3])*(coners[c1*3+(2+idx)%3] - coners[i*3+(1+idx)%3]); ++idx;
-		crossprod[3*i+idx] = (coners[c1*3+(1+idx)%3] - coners[i*3+(1+idx)%3])*(coners[c2*3+(2+idx)%3] - coners[i*3+(2+idx)%3])
-		- (coners[c2*3+(1+idx)%3] - coners[i*3+(1+idx)%3])*(coners[c1*3+(2+idx)%3] - coners[i*3+(1+idx)%3]);        
-		idx = 0;
-		scalarprod[i] = crossprod[3*i+idx]*(coners[c1*3+idx] - coners[i*3+idx]); ++idx;
-		scalarprod[i] += crossprod[3*i+idx]*(coners[c1*3+idx] - coners[i*3+idx]); ++idx;
-		scalarprod[i] += crossprod[3*i+idx]*(coners[c1*3+idx] - coners[i*3+idx]); 
-		
-		normcrossprod[i] = crossprod[3*i+0]*crossprod[3*i+0] + crossprod[3*i+1]*crossprod[3*i+1] +crossprod[3*i+2]*crossprod[3*i+2];
+
+        a[0] = vertex[c1][0] - vertex[i][0]; a[1] = vertex[c1][1] - vertex[i][1];  a[2] = vertex[c1][2] - vertex[i][2]; 
+        b[0] = vertex[c2][0] - vertex[i][0]; b[1] = vertex[c2][1] - vertex[i][1];  b[2] = vertex[c2][2] - vertex[i][2]; 
+        
+        compute_cross_product3(a,b,crossprods[i]);
+        scalarprod[i] = compute_dot_product3(crossprods[i],a);
+		normcrossprod[i] = compute_norm3(crossprods[i]);
 	}
 	for(i = 0; i<4; i++){
 		if(scalarprod[i] > 0.0 && scalarprod[(i+2)%4] > 0.0){
@@ -257,23 +251,24 @@ double compute_quadrilateral_area_(double coners[])
  Given three vectors a,b,c, compute ||c.(a x b)|| = |a|*|b|*|c|*cos(alpha)*sin(theta),
  it returns cossin[0] = cos(alpha); cossin[1] = sin(theta)
  */
-void compute_cossin(double vectors[], double cossin[])
+void compute_cossin(double vectors[][3], double cossin[])
 {
-	int idx;
-	double crossprod[3], scalarprod, ncross, n0, n1, n2;
+	double crossprod[3], a[3], b[3], c[3], scalarprod, ncross, n0, n1, n2;
 	
-	idx = 0;
-	crossprod[idx] = vectors[0*3+(1+idx)%3]*vectors[1*3+(2+idx)%3] - vectors[1*3+(1+idx)%3]*vectors[0*3+(2+idx)%3]; ++idx;
-	crossprod[idx] = vectors[0*3+(1+idx)%3]*vectors[1*3+(2+idx)%3] - vectors[1*3+(1+idx)%3]*vectors[0*3+(2+idx)%3]; ++idx;
-	crossprod[idx] = vectors[0*3+(1+idx)%3]*vectors[1*3+(2+idx)%3] - vectors[1*3+(1+idx)%3]*vectors[0*3+(2+idx)%3]; 
-	
-	
-	ncross = sqrt(crossprod[0]*crossprod[0] + crossprod[1]*crossprod[1] + crossprod[2]*crossprod[2]);
-	n0 = sqrt(vectors[0*3+0]*vectors[0*3+0] + vectors[0*3+1]*vectors[0*3+1] + vectors[0*3+2]*vectors[0*3+2]);
-	n1 = sqrt(vectors[1*3+0]*vectors[1*3+0] + vectors[1*3+1]*vectors[1*3+1] + vectors[1*3+2]*vectors[1*3+2]);
-	n2 = sqrt(vectors[2*3+0]*vectors[2*3+0] + vectors[2*3+1]*vectors[2*3+1] + vectors[2*3+2]*vectors[2*3+2]);
+    a[0] = vectors[0][0]; a[1] = vectors[0][1]; a[2] = vectors[0][2];
+    b[0] = vectors[1][0]; b[1] = vectors[1][1]; b[2] = vectors[1][2];
+    c[0] = vectors[2][0]; c[1] = vectors[2][1]; c[2] = vectors[2][2];
+    
+	compute_cross_product3(a,b,crossprod);    
+
+	ncross = compute_norm3(crossprod);
+	n0 = compute_norm3(a);
+	n1 = compute_norm3(b);
+	n2 = compute_norm3(c);
+    
 	//scalarprod = |c|.|a|.|b| sin(theta)*cos(alpha)
-	scalarprod = crossprod[0]*vectors[2*3+0] + crossprod[0]*vectors[2*3+1] + crossprod[0]*vectors[2*3+2];
+	
+    scalarprod = compute_dot_product3(crossprod,c);
 	cossin[1] = ncross/(n0*n1);
 	cossin[0] = scalarprod/(n0*n1*n2*cossin[1]); 
 }
@@ -532,7 +527,7 @@ PetscErrorCode DMDAComputeMeshQualityMetric_DiagonalRatio(DM dm,PetscReal *value
 }
 
 #undef __FUNCT__
-#define __FUNCT__ "DMDAComputeMeshQualityMetric_AreaRatio"
+#define __FUNCT__ "DMDAComputeMeshQualityMetric_FaceAreaRatio"
 PetscErrorCode DMDAComputeMeshQualityMetric_FaceAreaRatio(DM dm,PetscReal *value)
 {
 	DM              cda;
@@ -542,7 +537,7 @@ PetscErrorCode DMDAComputeMeshQualityMetric_FaceAreaRatio(DM dm,PetscReal *value
 	const PetscInt  *el_nidx;
 	PetscInt        *gidx;
 	PetscReal       el_coords[3*Q2_NODES_PER_EL_3D];
-	double          a1,a2,corners[3*4];
+	double          a1,a2,vertexes[4][3];
 	double          ratio,ratio_min,ratio_min_g;
 	PetscErrorCode  ierr;
 	
@@ -562,24 +557,24 @@ PetscErrorCode DMDAComputeMeshQualityMetric_FaceAreaRatio(DM dm,PetscReal *value
 	for (e=0;e<nel;e++) {
 		ierr = DMDAGetElementCoordinatesQ2_3D(el_coords,(PetscInt*)&el_nidx[nen*e],LA_gcoords);CHKERRQ(ierr);
         //Loops through face pairs.
-		get_face_coordinates(el_coords, Q2_FACE_WEST, corners);
-        a1 = compute_quadrilateral_area_(corners);
-        get_face_coordinates(el_coords, Q2_FACE_EAST, corners);
-		a2 = compute_quadrilateral_area_(corners);
+		get_face_coordinates(el_coords, Q2_FACE_WEST, vertexes);
+        a1 = compute_quadrilateral_area_(vertexes);
+        get_face_coordinates(el_coords, Q2_FACE_EAST, vertexes);
+		a2 = compute_quadrilateral_area_(vertexes);
         ratio = (a1<a2)?a1/a2:a2/a1;
         ratio_min = (ratio<ratio_min)?ratio:ratio_min;
 
-		get_face_coordinates(el_coords, Q2_FACE_SOUTH, corners);
-        a1 = compute_quadrilateral_area_(corners);
-        get_face_coordinates(el_coords, Q2_FACE_NORTH, corners);
-		a2 = compute_quadrilateral_area_(corners);
+		get_face_coordinates(el_coords, Q2_FACE_SOUTH, vertexes);
+        a1 = compute_quadrilateral_area_(vertexes);
+        get_face_coordinates(el_coords, Q2_FACE_NORTH, vertexes);
+		a2 = compute_quadrilateral_area_(vertexes);
         ratio = (a1<a2)?a1/a2:a2/a1;
         ratio_min = (ratio<ratio_min)?ratio:ratio_min;        
         
-		get_face_coordinates(el_coords, Q2_FACE_BACK, corners);
-        a1 = compute_quadrilateral_area_(corners);
-        get_face_coordinates(el_coords, Q2_FACE_FRONT, corners);
-		a2 = compute_quadrilateral_area_(corners);
+		get_face_coordinates(el_coords, Q2_FACE_BACK, vertexes);
+        a1 = compute_quadrilateral_area_(vertexes);
+        get_face_coordinates(el_coords, Q2_FACE_FRONT, vertexes);
+		a2 = compute_quadrilateral_area_(vertexes);
         ratio = (a1<a2)?a1/a2:a2/a1;
         ratio_min = (ratio<ratio_min)?ratio:ratio_min;            
 	}
@@ -593,8 +588,8 @@ PetscErrorCode DMDAComputeMeshQualityMetric_FaceAreaRatio(DM dm,PetscReal *value
 }
 
 #undef __FUNCT__
-#define __FUNCT__ "DMDAComputeMeshQualityMetric_CornerAngle"
-PetscErrorCode DMDAComputeMeshQualityMetric_CornerAngle(DM dm,PetscReal *value)
+#define __FUNCT__ "DMDAComputeMeshQualityMetric_VertexAngle"
+PetscErrorCode DMDAComputeMeshQualityMetric_VertexAngle(DM dm,PetscReal *value)
 {
 	DM              cda;
 	Vec             gcoords;
@@ -603,7 +598,7 @@ PetscErrorCode DMDAComputeMeshQualityMetric_CornerAngle(DM dm,PetscReal *value)
 	const PetscInt  *el_nidx;
 	PetscInt        *gidx;
 	PetscReal       el_coords[3*Q2_NODES_PER_EL_3D];
-	double          val, val_min, val_min_g, cossin[2], vectors[3*3];
+	double          val, val_min, val_min_g, cossin[2], vectors[3][3];
 	PetscErrorCode  ierr;
 	
 	PetscFunctionBegin;
@@ -620,43 +615,43 @@ PetscErrorCode DMDAComputeMeshQualityMetric_CornerAngle(DM dm,PetscReal *value)
 	val_min = 2.0; 
 	for (e=0;e<nel;e++) {
 	    ierr = DMDAGetElementCoordinatesQ2_3D(el_coords,(PetscInt*)&el_nidx[nen*e],LA_gcoords);CHKERRQ(ierr);
-        //Loops through corners
-        get_corner_vectors(el_coords,Q2_VERTEX_0,vectors);
+        //Loops through vertexes
+        get_vertex_vectors(el_coords,Q2_VERTEX_0,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val;
         
-        get_corner_vectors(el_coords,Q2_VERTEX_1,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_1,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val;
         
-        get_corner_vectors(el_coords,Q2_VERTEX_2,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_2,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val; 
         
-        get_corner_vectors(el_coords,Q2_VERTEX_3,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_3,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val; 
         
-        get_corner_vectors(el_coords,Q2_VERTEX_4,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_4,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val; 
         
-        get_corner_vectors(el_coords,Q2_VERTEX_5,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_5,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val; 
         
-        get_corner_vectors(el_coords,Q2_VERTEX_6,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_6,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val; 
         
-        get_corner_vectors(el_coords,Q2_VERTEX_7,vectors);
+        get_vertex_vectors(el_coords,Q2_VERTEX_7,vectors);
         compute_cossin(vectors,cossin); 
         val = (cossin[0]<cossin[1])?cossin[0]:cossin[1];
         val_min = (val_min<val)?val_min:val; 
@@ -692,8 +687,8 @@ PetscErrorCode DMDAComputeMeshQualityMetric(DM dm,MeshQualityMeasure measure,Pet
 			ierr = DMDAComputeMeshQualityMetric_DiagonalRatio(dm,value);CHKERRQ(ierr);
 			break;
         
-		case MESH_QUALITY_CORNER_ANGLE:
-			ierr = DMDAComputeMeshQualityMetric_CornerAngle(dm,value);CHKERRQ(ierr);
+		case MESH_QUALITY_VERTEX_ANGLE:
+			ierr = DMDAComputeMeshQualityMetric_VertexAngle(dm,value);CHKERRQ(ierr);
 			break;
     
     case MESH_QUALITY_FACE_AREA_RATIO:
