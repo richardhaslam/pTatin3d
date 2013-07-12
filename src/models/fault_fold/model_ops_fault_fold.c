ba29d18a958d47392b440aaaeb0b406ccd991d9f
svnuser <svnuser@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-07-12 08:00:36 +0000

    not symmetric perturbation
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@4570 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/models/fault_fold/model_ops_fault_fold.c
+++ src/models/fault_fold/model_ops_fault_fold.c
@@ -674,6 +674,45 @@ PetscErrorCode FaultFoldSetPerturbedInterfaces(DM dav, void *ctx)
                 }
                 break;
                 }
+                case 6://not centered perturbation
+                { 
+                attenuation = -log(0.01)/(PetscScalar)(data->perturbation_width);
+                fold_centers = data->fold_centers;
+                fold_center_front = 0.25*Lx + 0.5*Lx*fold_centers[0]/fold_centers[1];
+                fold_center_back = 0.25*Lx + 0.5*Lx*fold_centers[2]/fold_centers[3];
+                dy = data->Ly/(PetscScalar)(N/2-1);
+                psig1 = 0.25*lamb;
+                psig2 = psig1;
+                
+                for(i = si; i<si+nx; i++) {
+                    if((sj+ny == N) && (sj == 0)){
+                        PetscScalar center = 0;
+                        j=sj+ny-1;
+                        center = LA_coord[kinter][j][i].x-fold_center_back;
+                        pertu = amp * H * exp(-0.5*(center)*(center)/(psig1*psig1));
+                        for(pwidth = 0; pwidth<data->perturbation_width; pwidth++){
+                            LA_coord[kinter][j-pwidth][i].z += pertu *exp(-pwidth*attenuation);
+                        }
+                        j=0;
+                        pertu = amp * H *exp(-0.5*(center+offset*lamb)*(center+offset*lamb)/(psig2*psig2));
+                        for(pwidth = 0; pwidth<data->perturbation_width; pwidth++){
+                            LA_coord[kinter][j+pwidth][i].z += pertu *exp(-pwidth*attenuation);
+                        }
+                    }else if ((sj+ny == N) || (sj == 0)){
+                        PetscScalar center = 0;
+                        PetscInt sgn = 1;
+                        j= (sj == 0)?0:(sj+ny-1);
+                        sgn = (sj == 0)?1:-1;
+                        
+                        center = (sj == 0)?(LA_coord[kinter][j][i].x-fold_center_front):(LA_coord[kinter][j][i].x-fold_center_back);
+                        pertu = (sj == 0)?(amp * H *exp(-0.5*(center+offset*lamb)*(center+offset*lamb)/(psig2*psig2))):amp * H * exp(-0.5*(center)*(center)/(psig1*psig1));
+                        for(pwidth = 0; pwidth<data->perturbation_width; pwidth++){
+                            LA_coord[kinter][j+sgn*pwidth][i].z += pertu *exp(-pwidth*attenuation);
+                        }
+                    }
+                }
+                break;
+            }                
             }
                 
         
