b380d39af07d9cb3895c7b2700b4d895466406e9
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-09-23 11:02:48 +0000

    added unit tests for pt3d
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@4700 26174531-3d5a-4d31-aba0-7a6658d032cb

---
+++ src/unit_tests/ptatin3d-execute-tests.py
@@ -0,0 +1,140 @@
+
+import sys
+import os
+
+
+os.system('export PTATIN3D_DIR=`pwd`/..')
+
+
+def strip_keywords(infilename,outfilename):
+	bad_words = ['(MB)', '(sec)']
+	bad_words = bad_words + ['**']
+	bad_words = bad_words + ['[pTatinModel]']
+	bad_words = bad_words + ['[[']
+
+	#with open('t1.out') as oldfile, open('t1.strip.out', 'w') as newfile:
+
+	oldfile = open(infilename,'r')
+	newfile = open(outfilename,'w')
+
+	for line in oldfile:
+		if not any(bad_word in line for bad_word in bad_words):
+			newfile.write(line)
+			
+	oldfile.close()
+	newfile.close()
+
+
+#
+# Test definitions
+#
+
+# 1/ Check mf operators are matching assembled operators
+def test01():
+	test_name = 'test01'
+	ofile     = './output/'   + test_name + '.output'
+	efile     = './expected/' + test_name + '.expected'
+	optsfile  = './input/'    + test_name + '.opts'
+	print '--------------------------------------------------------'
+	print '-- Performing test:',test_name
+	os.system('${PTATIN3D_DIR}/test_stokes_operators.app -options_file ' + optsfile + ' > ' + ofile)
+	# Filter 
+	strip_keywords(ofile,ofile+'.strip')
+	strip_keywords(efile,'expected/expected.strip')
+	# DIFFERENCES
+	print '---- Unix diff with expected file:',efile
+	os.system('diff ' + ofile+'.strip expected/expected.strip') 
+
+
+# 2/ Check viscous block solve 
+def test02():
+	test_name = 'test02'
+	ofile     = './output/'   + test_name + '.output'
+	efile     = './expected/' + test_name + '.expected'
+	optsfile  = './input/'    + test_name + '.opts'
+	print '--------------------------------------------------------'
+	print '-- Performing test:',test_name
+	os.system('${PTATIN3D_DIR}/test_stokes_operators.app -options_file ' + optsfile + ' > ' + ofile)
+	# Filter 
+	strip_keywords(ofile,ofile+'.strip')
+	strip_keywords(efile,'expected/expected.strip')
+	# DIFFERENCES
+	print '---- Unix diff with expected file:',efile
+	os.system('diff ' + ofile+'.strip expected/expected.strip') 
+
+
+# 3a/ Linear solve GalerkinMG
+def test03a():
+	test_name = 'test03a'
+	ofile     = './output/'   + test_name + '.output'
+	efile     = './expected/' + test_name + '.expected'
+	optsfile  = './input/'    + test_name + '.opts'
+	print '--------------------------------------------------------'
+	print '-- Performing test:',test_name
+	os.system('${PTATIN3D_DIR}/ptatin_driver_linear_ts.app -options_file ' + optsfile + ' > ' + ofile)
+	# Filter 
+	strip_keywords(ofile,ofile+'.strip')
+	strip_keywords(efile,'expected/expected.strip')
+	# DIFFERENCES
+	print '---- Unix diff with expected file:',efile
+	os.system('diff ' + ofile+'.strip expected/expected.strip') 
+
+
+# 3b/ Linear solve MFMG
+def test03b():
+	test_name = 'test03b'
+	ofile     = './output/'   + test_name + '.output'
+	efile     = './expected/' + test_name + '.expected'
+	optsfile  = './input/'    + test_name + '.opts'
+	print '--------------------------------------------------------'
+	print '-- Performing test:',test_name
+	os.system('${PTATIN3D_DIR}/ptatin_driver_linear_ts.app -options_file ' + optsfile + ' > ' + ofile)
+	# Filter 
+	strip_keywords(ofile,ofile+'.strip')
+	strip_keywords(efile,'expected/expected.strip')
+	# DIFFERENCES
+	print '---- Unix diff with expected file:',efile
+	os.system('diff ' + ofile+'.strip expected/expected.strip') 
+
+# 3c/ Linear solve using MF,ASM,Galerkin and user specified level and direction dependent coarsening 
+def test03c():
+	test_name = 'test03c'
+	ofile     = './output/'   + test_name + '.output'
+	efile     = './expected/' + test_name + '.expected'
+	optsfile  = './input/'    + test_name + '.opts'
+	print '--------------------------------------------------------'
+	print '-- Performing test:',test_name
+	os.system('${PTATIN3D_DIR}/ptatin_driver_linear_ts.app -options_file ' + optsfile + ' > ' + ofile)
+	# Filter 
+	strip_keywords(ofile,ofile+'.strip')
+	strip_keywords(efile,'expected/expected.strip')
+	# DIFFERENCES
+	print '---- Unix diff with expected file:',efile
+	os.system('diff ' + ofile+'.strip expected/expected.strip') 
+
+
+
+# 4/ SNES - MFMG
+
+
+
+# Execute all tests
+def main():
+	print '**'
+	print '** Executing pTatin3d unit tests'
+	print '**'
+
+	# remove contents of directory where all ptatin output is sent
+	os.system('rm -f pt3dout/*')
+	os.system('rm -f output/*')
+
+#	test01()
+#	test02()
+#	test03a()
+#	test03b()
+	test03c()
+
+
+if __name__ == "__main__":
+	main()
+
