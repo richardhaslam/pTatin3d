68a8e7fdcea40f44900483da2125ec697842b57a
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-10-17 12:52:30 +0000

    updated headers
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@4867 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/geometry_object.h
+++ src/geometry_object.h
@@ -1,3 +1,37 @@
+/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ **
+ **    Copyright (c) 2012, 
+ **        Dave A. May [dave.may@erdw.ethz.ch]
+ **        Geophysical Fluid Dynamics, 
+ **        Department of Earth Sciences,
+ **        ETH Zürich,
+ **        Sonneggstrasse 5,
+ **        CH-8092 Zurich,
+ **        Switzerland
+ **
+ **    Project:       pTatin3d
+ **    Filename:      geometry_object.h
+ **
+ **
+ **    pTatin3d is free software: you can redistribute it and/or modify
+ **    it under the terms of the GNU General Public License as published by
+ **    the Free Software Foundation, either version 3 of the License, or
+ **    (at your option) any later version.
+ **
+ **    pTatin3d is distributed in the hope that it will be useful,
+ **    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ **    GNU General Public License for more details.
+ **
+ **    You should have received a copy of the GNU General Public License
+ **    along with pTatin3d.  If not, see <http://www.gnu.org/licenses/>.
+ **
+ **
+ **    $Id$
+ **
+ ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@*/
+
+
 #ifndef __geometry_object_h__
 #define __geometry_object_h__
 
@@ -125,4 +159,5 @@ PetscErrorCode GeometryObjectSetType_InfLayer(GeometryObject go,double x0[],doub
 PetscErrorCode GeomTypeNameGetId(const char name[],int *id);
 PetscErrorCode GeometryObjectFindByName(GeometryObject G[],const char name[],GeometryObject *g);
 PetscErrorCode GeometryObjectIdFindByName(GeometryObject G[],const char name[],PetscInt *GoId);
+
 #endif
