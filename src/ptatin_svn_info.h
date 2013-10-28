d26954b734f0e3bd6f05e1600da2ec2c54e78dac
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-10-28 09:22:08 +0000

    Updated version number
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@4896 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/ptatin_svn_info.h
+++ src/ptatin_svn_info.h
@@ -3,8 +3,8 @@
 #define __ptatin_svn_info_h__
 
   const char PTATIN_SVN_REPO_UUID[] = "Repository UUID: 26174531-3d5a-4d31-aba0-7a6658d032cb"; 
-  const char PTATIN_SVN_REPO_REV[] = "Revision: 4891"; 
-  const char PTATIN_SVN_REPO_LAST_CHANGE[] = "Log: r4891 | dmay | 2013-10-24 15:43:13 +0200 (Thu, 24 Oct 2013) | 1 line"; 
+  const char PTATIN_SVN_REPO_REV[] = "Revision: 4894"; 
+  const char PTATIN_SVN_REPO_LAST_CHANGE[] = "Log: r4894 | dmay | 2013-10-28 10:14:32 +0100 (Mon, 28 Oct 2013) | 1 line"; 
 
 #endif 
 
