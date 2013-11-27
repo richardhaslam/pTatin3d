105c97eb67175b50a296c70cd8ae6bae8c3e76b7
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-11-27 18:34:40 +0000

    updated externals
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@5006 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/externalpackages/interfaces/ptatin3d_external_packages.h
+++ src/externalpackages/interfaces/ptatin3d_external_packages.h
@@ -4,5 +4,13 @@
 
 PetscErrorCode ptatin3d_ApplyLandscapeEvolutionModel_SPMA(pTatinCtx pctx,Vec X);
 
+
+PetscErrorCode ptatin3d_ApplyLandscapeEvolutionModel_FastScape_V3(pTatinCtx pctx,Vec X,
+																																	PetscInt refinement_factor,
+																																	PetscReal Lstar,PetscReal Vstar,
+																																	PetscReal dt_mechanical,
+																																	int _law,double _m,double _kf,double _kd,int _bc);
+
+
 #endif
 
