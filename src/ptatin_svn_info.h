0d143f1d0f69fb817f57fbd18f2e4310267b3595
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2013-12-17 20:05:21 +0000

    updated header
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@5041 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/ptatin_svn_info.h
+++ src/ptatin_svn_info.h
@@ -2,9 +2,9 @@
 #ifndef __ptatin_svn_info_h__
 #define __ptatin_svn_info_h__
 
-  const char PTATIN_SVN_REPO_UUID[] = "Repository UUID: 26174531-3d5a-4d31-aba0-7a6658d032cb"; 
-  const char PTATIN_SVN_REPO_REV[] = "Revision: 5014"; 
-  const char PTATIN_SVN_REPO_LAST_CHANGE[] = "Log: r5014 | dmay | 2013-11-28 19:05:36 +0100 (Thu, 28 Nov 2013) | 1 line"; 
+  #define PTATIN_SVN_REPO_UUID        "Repository UUID: 26174531-3d5a-4d31-aba0-7a6658d032cb"
+  #define PTATIN_SVN_REPO_REV         "Revision: 5040"
+  #define PTATIN_SVN_REPO_LAST_CHANGE "Log: r5040 | dmay | 2013-12-17 21:03:16 +0100 (Tue, 17 Dec 2013) | 1 line"
 
 #endif 
 
