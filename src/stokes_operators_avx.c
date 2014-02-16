647972b1a93274a6492785bde6f5d8f53bbb78ee
Jed Brown <jed@jedbrown.org>
2014-02-16 15:32:48 -0700

    AVX: optimize TensorContractNEV_AVX (32 GF on my i7-4500U)

--- src/stokes_operators_avx.c
+++ src/stokes_operators_avx.c
@@ -38,11 +38,15 @@ static PetscErrorCode TensorContractNEV_AVX(PetscReal Rf[][3],PetscReal Sf[][3],
 	// u[l,k,j,c] = R[c,i] x[l,k,j,i]
 	PetscMemzero(u,sizeof u);
 	for (PetscInt l=0; l<3; l++) {
-		for (PetscInt kj=0; kj<9; kj++) {
-			for (PetscInt i=0; i<3; i++) {
-				for (PetscInt c=0; c<3; c++) {
-					for (PetscInt e=0; e<NEV; e++) u[l][kj*3+c][e] += R[c][i] * x[l][kj*3+i][e];
+		for (PetscInt c=0; c<3; c++) {
+			__m256d r[3] = {_mm256_set1_pd(R[c][0]),_mm256_set1_pd(R[c][1]),_mm256_set1_pd(R[c][2])};
+			for (PetscInt kj=0; kj<9; kj++) {
+				__m256d u_lkjc = _mm256_load_pd(u[l][kj*3+c]);
+				for (PetscInt i=0; i<3; i++) {
+					__m256d x_lkji = _mm256_load_pd(x[l][kj*3+i]);
+					u_lkjc = _mm256_fmadd_pd(r[i],x_lkji,u_lkjc);
 				}
+				_mm256_store_pd(u[l][kj*3+c],u_lkjc);
 			}
 		}
 	}
@@ -51,23 +55,31 @@ static PetscErrorCode TensorContractNEV_AVX(PetscReal Rf[][3],PetscReal Sf[][3],
 	PetscMemzero(v,sizeof v);
 	for (PetscInt l=0; l<3; l++) {
 		for (PetscInt k=0; k<3; k++) {
-			for (PetscInt j=0; j<3; j++) {
+			for (PetscInt b=0; b<3; b++) {
+				__m256d s[3] = {_mm256_set1_pd(S[b][0]),_mm256_set1_pd(S[b][1]),_mm256_set1_pd(S[b][2])};
 				for (PetscInt c=0; c<3; c++) {
-					for (PetscInt b=0; b<3; b++) {
-						for (PetscInt e=0; e<NEV; e++) v[l][(k*3+b)*3+c][e] += S[b][j] * u[l][(k*3+j)*3+c][e];
+					__m256d v_lkbc = _mm256_load_pd(v[l][(k*3+b)*3+c]);
+					for (PetscInt j=0; j<3; j++) {
+						__m256d u_lkjc = _mm256_load_pd(u[l][(k*3+j)*3+c]);
+						v_lkbc = _mm256_fmadd_pd(s[j],u_lkjc,v_lkbc);
 					}
+					_mm256_store_pd(v[l][(k*3+b)*3+c],v_lkbc);
 				}
 			}
 		}
 	}
 
 	// y[l,a,b,c] = T[a,k] v[l,k,b,c]
-	for (PetscInt k=0; k<3; k++) {
-		for (PetscInt l=0; l<3; l++) {
-			for (PetscInt a=0; a<3; a++) {
-				for (PetscInt ji=0; ji<9; ji++) {
-					for (PetscInt e=0; e<NEV; e++) y[l][a*9+ji][e] += T[a][k] * v[l][k*9+ji][e];
+	for (PetscInt a=0; a<3; a++) {
+		__m256d t[3] = {_mm256_set1_pd(T[a][0]),_mm256_set1_pd(T[a][1]),_mm256_set1_pd(T[a][2])};
+		for (PetscInt ji=0; ji<9; ji++) {
+			for (PetscInt l=0; l<3; l++) {
+				__m256d y_laji = _mm256_load_pd(y[l][a*9+ji]);
+				for (PetscInt k=0; k<3; k++) {
+					__m256d v_lkji = _mm256_load_pd(v[l][k*9+ji]);
+					y_laji = _mm256_fmadd_pd(v_lkji,t[k],y_laji);
 				}
+				_mm256_store_pd(y[l][a*9+ji],y_laji);
 			}
 		}
 	}
