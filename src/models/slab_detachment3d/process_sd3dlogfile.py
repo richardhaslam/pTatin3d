fbdacd2fe995b662a7b3e3cbd089669f402f2664
dmay <dmay@26174531-3d5a-4d31-aba0-7a6658d032cb>
2014-05-13 18:10:58 +0000

    updated post processing script to accomodate new fields
    git-svn-id: svn+ssh://musashi.ethz.ch/var/svn/davemay/Codes/tatin/ptatin3d@5599 26174531-3d5a-4d31-aba0-7a6658d032cb

--- src/models/slab_detachment3d/process_sd3dlogfile.py
+++ src/models/slab_detachment3d/process_sd3dlogfile.py
@@ -13,13 +13,16 @@ print '# === slab_detachment3d post processing ', sys.argv[1], '=== #'
 filename = sys.argv[1]
 file = open( filename, 'r' )
 
-#        0       1         2       3     4     5     6       7       8       9      10
-key = { 'step', 'timeNd', 'time', 'Ys', 'Yn', 'Xm', 'Ymin', 'Ymax', 'Vrms', 'Phi', 'vol' }
+outfile = open('sd3d_bmark.dat','w')
+
+#        0       1         2       3     4          5          6          7          8           9           10      11      12      13     14
+key = { 'step', 'timeNd', 'time', 'Ys', 'Xn_edge', 'Yn_edge', 'Xn_back', 'Yn_back', 'Zn_front', 'Yn_front', 'Ymin', 'Ymax', 'Vrms', 'Phi', 'vol' }
 
 
-W0 = 40.0 * 1.0e3 # half slab width
+W0x = 40.0 * 1.0e3 # half slab width
+W0z = 250.0 * 1.0e3 # slab length (in the third dimension(
 tc = 22.56 * 1.0e6 * ( 3600.0 * 24.0 * 365.0 ); # 22.56 Ma in sec
-Vc = W0 / tc
+Vc = W0x / tc
 Ly = 660.0 * 1.0e3
 
 # initial volume
@@ -28,7 +31,7 @@ V0 = 660.0e3 * 500.0e3 * 500.0e3
 scan_remainder = False
 
 for line in file:
-    x = line.find('# step |')
+    x = line.find('|             step |')
     if x != -1:
         scan_remainder = True
         continue
@@ -42,26 +45,44 @@ for line in file:
         idx = 3 # Ds
         Ds_star = (Ly - float(entries[idx]))/Ly
 
-        idx = 4 # Dn
+        # edge values
+        idx = 4 # W - I measured max x coordinate of the slab, so this needs to be rescaled
+        W_star = (500.0e3 - float(entries[idx]))/W0x
+
+        idx = 5 # Dn
         DN_star = (Ly - float(entries[idx]))/Ly
 
-        idx = 5 # W - I measured max x coordinate of the slab, so this needs to be rescaled
-        W_star = (500.0e3 - float(entries[idx]))/W0
+        # back face values (y=0 plane in benchmark, z=0 plane in ptatin)
+        idx = 6 # W - I measured max x coordinate of the slab, so this needs to be rescaled
+        Wn_back_star = (500.0e3 - float(entries[idx]))/W0x
 
-        idx = 6 # Ymin/Ymax
-        Zmin_star = (Ly - float(entries[idx]))/Ly
         idx = 7
-        Z_max = (Ly - float(entries[idx]))/Ly
+        DN_back_star = (Ly - float(entries[idx]))/Ly
 
-        idx = 8 # vrms
+        # front face values (x=Lx plane in benchmark, z=500 km plane in ptatin)
+        idx = 8
+        Wn_front_star = (float(entries[idx]))/W0z
+        idx = 9
+        DN_front_star = (Ly - float(entries[idx]))/Ly
+
+        idx = 10 # Ymin/Ymax
+        Zmin_star = (Ly - float(entries[idx]))/Ly
+        idx = 11
+        Zmax_star = (Ly - float(entries[idx]))/Ly
+
+        idx = 12 # vrms
         Vrms_star = float(entries[idx])/Vc
 
-        idx = 9 # dissipation - scale by what??
+        idx = 13 # dissipation - SI units
         phi_star = float(entries[idx])
 
-        idx = 10 # vol
+        idx = 14 # vol
         vol_star = (float(entries[idx]) - V0)/V0
 
+#        print '%.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e' % (time_star,Ds_star, W_star,DN_star, Wn_back_star,DN_back_star, Wn_front_star,DN_front_star, Zmin_star,Zmax_star, Vrms_star,phi_star,vol_star)
+
+        outfile.write('%.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e %.12e\n' % (time_star,Ds_star, W_star,DN_star, Wn_back_star,DN_back_star, Wn_front_star,DN_front_star, Zmin_star,Zmax_star, Vrms_star,phi_star,vol_star))
 
 
 file.close()
+outfile.close()
