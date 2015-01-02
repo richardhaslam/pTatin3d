a178c99453f356acb23afccc2977790c6028f464
dmay <dave.mayhem23@gmail.com>
2015-01-02 12:56:33 +0100

    Updated header in models I wrote

--- src/models/gene3d/model_gene3d_ctx.h
+++ src/models/gene3d/model_gene3d_ctx.h
@@ -1,35 +1,31 @@
-/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  **
- **    Copyright (c) 2012, 
+ **    Copyright (c) 2012
  **        Dave A. May [dave.may@erdw.ethz.ch]
- **        Geophysical Fluid Dynamics, 
- **        Department of Earth Sciences,
- **        ETH Zürich,
- **        Sonneggstrasse 5,
- **        CH-8092 Zurich,
+ **        Institute of Geophysics
+ **        ETH Zürich
+ **        Sonneggstrasse 5
+ **        CH-8092 Zürich
  **        Switzerland
  **
- **    Project:       pTatin3d
- **    Filename:      model_gene3d_ctx.h
+ **    project:    pTatin3d
+ **    filename:   model_gene3d_ctx.h
  **
  **
  **    pTatin3d is free software: you can redistribute it and/or modify
- **    it under the terms of the GNU General Public License as published by
- **    the Free Software Foundation, either version 3 of the License, or
- **    (at your option) any later version.
+ **    it under the terms of the GNU General Public License as published
+ **    by the Free Software Foundation, either version 3 of the License,
+ **    or (at your option) any later version.
  **
  **    pTatin3d is distributed in the hope that it will be useful,
  **    but WITHOUT ANY WARRANTY; without even the implied warranty of
- **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- **    GNU General Public License for more details.
+ **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ **    See the GNU General Public License for more details.
  **
  **    You should have received a copy of the GNU General Public License
- **    along with pTatin3d.  If not, see <http://www.gnu.org/licenses/>.
+ **    along with pTatin3d. If not, see <http://www.gnu.org/licenses/>.
  **
- **
- **    $Id$
- **
- ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~@*/
+ ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ @*/
 
 
 #ifndef __ptatinmodel_GENE3D_ctx_h__
