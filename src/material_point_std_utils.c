c4e9e6c08fe604e9778e79ed52e54880d24439e1
Dave May <dave.mayhem23@gmail.com>
2015-04-22 15:24:35 +0200

    Fixed casting

--- src/material_point_std_utils.c
+++ src/material_point_std_utils.c
@@ -1283,7 +1283,7 @@ PetscErrorCode SwarmMPntStd_CoordAssignment_InsertWithinPlane(DataBucket db,DM d
             InverseMappingDomain_3dQ2(tolerance,max_its,
                                       use_nonzero_guess,
                                       monitor,
-                                      (const double*)LA_gcoords, (const int)lmx,(const int)lmy,(const int)lmz, (const int*)elnidx_u,
+                                      (const PetscReal*)LA_gcoords, (const PetscInt)lmx,(const PetscInt)lmy,(const PetscInt)lmz, (const PetscInt*)elnidx_u,
                                       1, &mp_std );
             
             point_on_edge = PETSC_FALSE;
