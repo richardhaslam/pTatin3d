91fd41ef786c3ba21e34fb288d7f786884c8f472
gduclaux <guillaume.duclaux@geo.uib.no>
2015-09-21 16:13:58 +0200

    Energy constants: * Compile source for energy constants (partially auto-generated)

--- src/material_constants/local.mk
+++ src/material_constants/local.mk
@@ -10,4 +10,8 @@ libptatin3d-y.c += $(call thisdir, \
 			MaterialConst_SoftExpo_def.c \
 			MaterialConst_PlasticMises_def.c \
 			MaterialConst_PlasticDP_def.c \
+			EnergyMaterialConstants_def.c \
+                        EnergySourceConst_def.c \
+			EnergySourceDecay_def.c \
+			EnergySourceAdiabaticAdvection_def.c \
 	)
