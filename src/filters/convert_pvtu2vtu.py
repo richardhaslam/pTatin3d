b6e640d1ccdd8eb235ef44b3eaf1c7d205e26f62
Dave May <dave.mayhem23@gmail.com>
2015-09-28 12:23:13 +0200

    pvpython filter: * Updated for paraview 4.3.1

--- src/filters/convert_pvtu2vtu.py
+++ src/filters/convert_pvtu2vtu.py
@@ -1,31 +1,33 @@
 
+import os
 import sys
 from optparse import OptionParser
 
 
-from paraview.servermanager import *
+from paraview import simple
+#from paraview.servermanager import *
 
 
 def convert_pvtu2vtu(infilename,outfilename):
 	# Unstructured Grid (particles)
-	reader   = sources.XMLPUnstructuredGridReader(FileName=infilename)
+	reader   = simple.XMLPartitionedUnstructuredGridReader(FileName=infilename)
 
 	# DataMode = {0,1,2} => {ascii, encoded, appended}
-	writer   = writers.XMLUnstructuredGridWriter(Input=reader,DataMode=1,FileName=outfilename)
+	writer   = simple.XMLUnstructuredGridWriter(Input=reader,DataMode=1,FileName=outfilename)
 
 	writer.UpdatePipeline()
 
 
 def main():
-	Connect()
+	#Connect()
 
 	optparser=OptionParser(usage='usage: %prog -i <filename1>',
-												 add_help_option=True,
-												 description="""Read particle vtu file """ + 
-												 """and write data to a single binary file.""")
+			add_help_option=True,
+			description="""Read parallel vtu (PVTU) file """ + 
+			"""and write data to a single binary VTU file.""")
 
 	optparser.add_option( "-i", "--input", dest="opt_inputfile",
-										help="Input file name", metavar="FILE")
+			help="Input file name", metavar="FILE")
 
 	(options, argv) = optparser.parse_args()
 
@@ -42,6 +44,12 @@ def main():
 		print 'Reading: ' + infilename
 		print 'Writing: ' + vts_name
 		convert_pvtu2vtu(infilename,vts_name)
+	else:
+		print('Warning: Input file specified is not a valid PVTU file')
+		print('Warning: A valid PVTU file must have the extension .pvtu')
+		print('Warning: Found input file: ' + infilename)
+		optparser.print_help()
+		sys.exit(1)
 
 
 if __name__ == '__main__':
