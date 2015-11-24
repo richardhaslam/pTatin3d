18cc319e20dd1ab4a97bbe88b07279e4f3b1e34f
Dave May <dave.mayhem23@gmail.com>
2015-11-24 10:21:53 +0100

    MarkerCellFields: * Added header

--- src/output_material_points_p0.c
+++ src/output_material_points_p0.c
@@ -1,4 +1,31 @@
-
+/*@ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ **
+ **    Copyright (c) 2012
+ **        Dave A. May [dave.may@erdw.ethz.ch]
+ **        Institute of Geophysics
+ **        ETH Zürich
+ **        Sonneggstrasse 5
+ **        CH-8092 Zürich
+ **        Switzerland
+ **
+ **    project:    pTatin3d
+ **    filename:   output_material_points_p0.c
+ **
+ **
+ **    pTatin3d is free software: you can redistribute it and/or modify
+ **    it under the terms of the GNU General Public License as published
+ **    by the Free Software Foundation, either version 3 of the License,
+ **    or (at your option) any later version.
+ **
+ **    pTatin3d is distributed in the hope that it will be useful,
+ **    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ **    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ **    See the GNU General Public License for more details.
+ **
+ **    You should have received a copy of the GNU General Public License
+ **    along with pTatin3d. If not, see <http://www.gnu.org/licenses/>.
+ **
+ ** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ @*/
 
 #include "petsc.h"
 
@@ -25,7 +52,7 @@
 #include "energy_output.h"
 
 #include "output_material_points.h"
-//#include "output_material_points_p0.h"
+#include "output_material_points_p0.h"
 
 #undef __FUNCT__
 #define __FUNCT__ "MarkerCellFieldsP0_ProjectIntegerField"
