639fba77566fdf63c0b9c961ee5c865a69343a4d
Patrick Sanan <patrick.sanan@gmail.com>
2017-06-30 12:41:20 +0200

    WIP: pyTestHarness integration
    Update the tests to use our pyTestHarness system.
    
    The existing test can now be run on a local machine by executing ./runTest.py
    from the tests/ directory.
    
    Remaining TODOs:
    * Define groups of tests, as described in runTest.py
    * Allow for parallel execution of tests

---
+++ tests/runTests.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+#################################################################################
+#                                                                               #
+#                                pTatin3D Tests                                 #
+#                                                                               #
+#################################################################################
+# Usage:                                                                        #
+#  ./runTests.py [-h] [-other_pyTestHarness_options] [--group <groupname>]       #
+#                     [--list_groups]                                           #
+#                                                                               #
+# Design principles:                                                            #
+# * It should be easy to add, move, or rename tests                             #
+# * It should be easy to run a meaningful subset of tests                       #
+# * Tests should have descriptive names                                         #
+#                                                                               #
+# Useful test groups:                                                           #
+#  --group all           : All tests (default).                                 #
+#  --group smoke         : Smoke tests. Quickly check minimal operation.        #
+#  --group unit          : Unit tests. Check first if code is broken.           #
+#  --group integration   : Integration tests. Check second if code is broken.   #
+#  --group verification  : Verification tests. If these pass, the code "works". #
+#  --group validation    : Validation tests.                                    #
+#  --group ci            : Tests to run during automated tests.                 #
+#  --group classic       : The previous tests.                                  #
+#################################################################################
+
+import pyTestHarness.harness as pyth_harness   # bitbucket.org/dmay/pyTestHarness
+import os
+
+# PTATIN_DIR and PETSC_ARCH are used to make easily-copyable tests
+thisDir = os.path.dirname(os.path.abspath(__file__))
+PTATIN_DIR = os.getenv('PTATIN_DIR')
+if not PTATIN_DIR :
+    print("You must define PTATIN_DIR in your environment. Exiting.")
+    exit(1)
+if os.path.join(PTATIN_DIR,'tests') != thisDir :
+    print("PTATIN_DIR is not set correctly in your environment. Exiting.")
+    exit(1)
+PETSC_ARCH = os.getenv('PETSC_ARCH')
+if not PETSC_ARCH :
+    print("You must define PETSC_ARCH in your environment. Exiting.")
+    exit(1)
+
+#  Interpret any child directory containing "test.py" as defining as a test
+#  with a name defined as the relative path to the containing directory.
+#  Each intermediate directory becomes a group.
+allTests = []
+# TODO: populate a list of groups here corresponding to subdirectories
+for (root, dirs, files) in os.walk(os.getcwd()) :
+    if 'test.py' in files :
+        relpath = os.path.relpath(root,thisDir)
+        mod = relpath.replace(os.sep,'.') + '.test'
+        exec('import ' + mod)
+        exec('allTests.append('+mod+'.test())')
+
+# TODO: parse command line args to define groups
+
+# Run tests
+# TODO: these need to be somehow sandboxed, so that output files (even if you don't think you're supposed to generate any) don't clash
+os.environ['PYTHONUNBUFFERED'] = str('1')
+launcher = pyth_harness.pthHarness(allTests)
+launcher.execute()
+exitcode = launcher.verify()
+launcher.clean()
+exit(exitcode)
---
+++ tests/verification/linear/sinker_hybrid_8x8x8/test.py
@@ -0,0 +1,26 @@
+import os
+import pyTestHarness.unittest as pth
+import pyTestHarness.launch as launch
+import re
+
+def test() :
+  PTATIN_DIR = os.getenv('PTATIN_DIR')
+  PETSC_ARCH = os.getenv('PETSC_ARCH')
+  thisDir = os.path.split(os.path.abspath(__file__))[0]
+  testName = os.path.split(thisDir)[1]
+  ranks = 1
+  launch = os.path.join(PTATIN_DIR,PETSC_ARCH,'bin','ptatin_driver_linear_ts.app') + ' -options_file ' + os.path.join(thisDir,'opts')
+  expectedFile = os.path.join(thisDir,'expected')
+
+  def comparefunc(t) :
+    tol = 1e-6
+    t.compareFloatingPoint(re.escape('0 KSP Residual norm'                 ),tol)
+    t.compareFloatingPoint(re.escape('1 KSP Residual norm'                 ),tol)
+    t.compareFloatingPoint(re.escape('10 KSP Residual norm'                ),tol)
+    t.compareFloatingPoint(re.escape('18 KSP Residual norm'                ),tol)
+
+  # Create Test Object
+  t = pth.pthUnitTest(testName,ranks,launch,expectedFile)
+  t.setVerifyMethod(comparefunc)
+
+  return(t)
