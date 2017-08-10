d064ad6b825fb71a0ec5545ca8dee0f6afc90b68
Dave May <dave.mayhem23@gmail.com>
2017-08-10 07:41:57 +0100

    MPIIO test: * Replaces #define for data length with cmd line arg * Added timing info to report bytes wrote/read per sec * Added pass/fail test for single write/read case

--- src/tests/test_mpiiob.c
+++ src/tests/test_mpiiob.c
@@ -32,28 +32,35 @@
 #include <ptatin_init.h>
 #include <mpiio_blocking.h>
 
-#define FAC 5
 
-PetscErrorCode test_single_write(PetscBool skip_header)
+PetscErrorCode test_single_write(PetscInt length_ref,PetscBool skip_header)
 {
   PetscErrorCode ierr;
   PetscMPIInt rank,root = 0;
   FILE *fp = NULL;
   double *data;
   int length,i;
+  long int bytes,bytes_sum_g;
+  PetscLogDouble t0,t1;
   
   ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);CHKERRQ(ierr);
   if (rank == root) {
     fp = fopen("test-ex1.bin","w");
   }
   
-  length = FAC + rank*3;
+  length = length_ref + rank*3;
   data = malloc(length*sizeof(double));
   for (i=0; i<length; i++) {
     data[i] = 100.0*rank + i;
   }
   
+  PetscTime(&t0);
   ierr = MPIWrite_Blocking(fp,data,length,sizeof(double),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscTime(&t1);
+  
+  bytes = length * sizeof(double);
+  ierr = MPI_Allreduce(&bytes,&bytes_sum_g,1, MPI_LONG, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscPrintf(PETSC_COMM_WORLD,".. [single][MPIWrite_Blocking] Wrote %1.2e (MB) in %1.2e (sec)\n",(double)bytes_sum_g/1.0e6,t1-t0);
   
   free(data);
   data = NULL;
@@ -64,13 +71,16 @@ PetscErrorCode test_single_write(PetscBool skip_header)
   PetscFunctionReturn(0);
 }
 
-PetscErrorCode test_single_read(PetscBool skip_header)
+PetscErrorCode test_single_read(PetscInt length_ref,PetscBool skip_header)
 {
   PetscErrorCode ierr;
   PetscMPIInt commsize,rank,r,root = 0;
   FILE *fp = NULL;
   double *data = NULL;
   int length,i,start;
+  long int bytes,bytes_sum_g;
+  double diff,diff_g;
+  PetscLogDouble t0,t1;
   
   ierr = MPI_Comm_size(MPI_COMM_WORLD,&commsize);CHKERRQ(ierr);
   ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);CHKERRQ(ierr);
@@ -78,17 +88,38 @@ PetscErrorCode test_single_read(PetscBool skip_header)
     fp = fopen("test-ex1.bin","r");
   }
   
-  length = FAC + rank*3;
+  length = length_ref + rank*3;
   start = 0;
   for (r=0; r<rank; r++) {
-    start += (FAC + r*3);
+    start += (length_ref + r*3);
   }
+
+  PetscTime(&t0);
   ierr = MPIRead_Blocking(fp,(void**)&data,length,sizeof(double),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscTime(&t1);
   
+  bytes = length * sizeof(double);
+  ierr = MPI_Allreduce(&bytes,&bytes_sum_g,1, MPI_LONG, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscPrintf(PETSC_COMM_WORLD,".. [single][MPIRead_Blocking] Read %1.2e (MB) in %1.2e (sec)\n",(double)bytes_sum_g/1.0e6,t1-t0);
+  
+  /*
   if (rank == (commsize-1)) {
     for (i=0; i<length; i++) {
       printf("[rank %d][%d] %1.4e\n",(int)rank,start+i,data[i]);
     }
+  }
+  */
+   
+  diff = 0.0;
+  for (i=0; i<length; i++) {
+    diff += fabs( data[i] - (double)(100.0*rank + i) );
+  }
+  ierr = MPI_Allreduce(&diff,&diff_g,1, MPI_DOUBLE, MPI_MAX, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  
+  if (diff_g < 1.0e-12) {
+    PetscPrintf(PETSC_COMM_WORLD,"++ [single][Pass] Max difference between expected and data read is < 1.0e-12\n");
+  } else {
+    PetscPrintf(PETSC_COMM_WORLD,"-- [single][Fail] Max difference between expected and data read is %+1.4e\n",diff_g);
   }
   
   free(data);
@@ -100,7 +131,7 @@ PetscErrorCode test_single_read(PetscBool skip_header)
   PetscFunctionReturn(0);
 }
 
-PetscErrorCode test_multidata_write(PetscBool skip_header)
+PetscErrorCode test_multidata_write(PetscInt length_ref,PetscBool skip_header)
 {
   PetscErrorCode ierr;
   PetscMPIInt rank,root = 0;
@@ -109,12 +140,14 @@ PetscErrorCode test_multidata_write(PetscBool skip_header)
   int   *data2;
   short *data3;
   long int length[3],i;
+  long int bytes,bytes_sum_g;
+  PetscLogDouble t0,t1;
   
   ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);CHKERRQ(ierr);
   
-  length[0] = (rank+1)*3;
-  length[1] = (rank+2)*2;
-  length[2] = (rank+3)*1;
+  length[0] = length_ref + (rank+1)*3;
+  length[1] = length_ref + (rank+2)*2;
+  length[2] = length_ref + (rank+3)*1;
   
   data1 = malloc(length[0]*sizeof(double));
   data2 = malloc(length[1]*sizeof(int));
@@ -133,10 +166,19 @@ PetscErrorCode test_multidata_write(PetscBool skip_header)
   if (rank == root) {
     fp = fopen("test_multi.bin","w");
   }
+
+  PetscTime(&t0);
   ierr = MPIWrite_Blocking(fp,data1,length[0],sizeof(double),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
   ierr = MPIWrite_Blocking(fp,data2,length[1],sizeof(int),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
   ierr = MPIWrite_Blocking(fp,data3,length[2],sizeof(short),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscTime(&t1);
   
+  bytes  = length[0] * sizeof(double);
+  bytes += length[1] * sizeof(int);
+  bytes += length[2] * sizeof(short);
+  ierr = MPI_Allreduce(&bytes,&bytes_sum_g,1, MPI_LONG, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscPrintf(PETSC_COMM_WORLD,".. [multi][MPIWrite_Blocking x 3] Wrote %1.2e (MB) in %1.2e (sec)\n",(double)bytes_sum_g/1.0e6,t1-t0);
+
   free(data3);
   free(data2);
   free(data1);
@@ -147,7 +189,7 @@ PetscErrorCode test_multidata_write(PetscBool skip_header)
   PetscFunctionReturn(0);
 }
 
-PetscErrorCode test_multidata_read(PetscBool skip_header)
+PetscErrorCode test_multidata_read(PetscInt length_ref,PetscBool skip_header)
 {
   PetscErrorCode ierr;
   PetscMPIInt rank,root = 0;
@@ -157,19 +199,31 @@ PetscErrorCode test_multidata_read(PetscBool skip_header)
   short  *data3 = NULL;
   long int i,length[3];
   double diff[3],diff_g[3],diff1,diff2,diff3;
+  long int bytes,bytes_sum_g;
+  PetscLogDouble t0,t1;
   
   ierr = MPI_Comm_rank(MPI_COMM_WORLD,&rank);CHKERRQ(ierr);
   
-  length[0] = (rank+1)*3;
-  length[1] = (rank+2)*2;
-  length[2] = (rank+3)*1;
+  length[0] = length_ref + (rank+1)*3;
+  length[1] = length_ref + (rank+2)*2;
+  length[2] = length_ref + (rank+3)*1;
   
   if (rank == root) {
     fp = fopen("test_multi.bin","r");
   }
+  
+  PetscTime(&t0);
   ierr = MPIRead_Blocking(fp,(void**)&data1,length[0],sizeof(double),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
   ierr = MPIRead_Blocking(fp,(void**)&data2,length[1],sizeof(int),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
   ierr = MPIRead_Blocking(fp,(void**)&data3,length[2],sizeof(short),root,skip_header,PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscTime(&t1);
+  
+  bytes  = length[0] * sizeof(double);
+  bytes += length[1] * sizeof(int);
+  bytes += length[2] * sizeof(short);
+  ierr = MPI_Allreduce(&bytes,&bytes_sum_g,1, MPI_LONG, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  PetscPrintf(PETSC_COMM_WORLD,".. [multi][MPIRead_Blocking x 3] Read %1.2e (MB) in %1.2e (sec)\n",(double)bytes_sum_g/1.0e6,t1-t0);
+  
   
   diff1 = 0.0;
   for (i=0; i<length[0]; i++) {
@@ -192,19 +246,19 @@ PetscErrorCode test_multidata_read(PetscBool skip_header)
   ierr = MPI_Allreduce(diff,diff_g,3, MPI_DOUBLE, MPI_MAX, PETSC_COMM_WORLD);CHKERRQ(ierr);
   
   if (diff_g[0] < 1.0e-12) {
-    PetscPrintf(PETSC_COMM_WORLD,"++ [Pass] Max difference between expected and data read is < 1.0e-12 <field1>\n");
+    PetscPrintf(PETSC_COMM_WORLD,"++ [multi][Pass] Max difference between expected and data read is < 1.0e-12 <field1>\n");
   } else {
-    PetscPrintf(PETSC_COMM_WORLD,"++ [Fail] Max difference between expected and data read is %+1.4e <field1>\n",diff_g[0]);
+    PetscPrintf(PETSC_COMM_WORLD,"-- [multi][Fail] Max difference between expected and data read is %+1.4e <field1>\n",diff_g[0]);
   }
   if (diff_g[1] < 1.0e-12) {
-    PetscPrintf(PETSC_COMM_WORLD,"++ [Pass] Max difference between expected and data read is < 1.0e-12 <field2>\n");
+    PetscPrintf(PETSC_COMM_WORLD,"++ [multi][Pass] Max difference between expected and data read is < 1.0e-12 <field2>\n");
   } else {
-    PetscPrintf(PETSC_COMM_WORLD,"++ [Fail] Max difference between expected and data read is %+1.4e <field2>\n",diff_g[1]);
+    PetscPrintf(PETSC_COMM_WORLD,"-- [multi][Fail] Max difference between expected and data read is %+1.4e <field2>\n",diff_g[1]);
   }
   if (diff_g[2] < 1.0e-12) {
-    PetscPrintf(PETSC_COMM_WORLD,"++ [Pass] Max difference between expected and data read is < 1.0e-12 <field3>\n");
+    PetscPrintf(PETSC_COMM_WORLD,"++ [multi][Pass] Max difference between expected and data read is < 1.0e-12 <field3>\n");
   } else {
-    PetscPrintf(PETSC_COMM_WORLD,"++ [Fail] Max difference between expected and data read is %+1.4e <field3>\n",diff_g[2]);
+    PetscPrintf(PETSC_COMM_WORLD,"-- [multi][Fail] Max difference between expected and data read is %+1.4e <field3>\n",diff_g[2]);
   }
   
   
@@ -222,28 +276,30 @@ PetscErrorCode test_multidata_read(PetscBool skip_header)
 int main(int argc,char **argv)
 {
   PetscErrorCode ierr;
-  PetscInt       test_id;
+  PetscInt       test_id,length;
   
   ierr = pTatinInitialize(&argc,&argv,(char *)0,NULL);CHKERRQ(ierr);
   
   test_id = 1;
   ierr = PetscOptionsGetInt(NULL,NULL,"-test_id",&test_id,NULL);CHKERRQ(ierr);
+  length = 5;
+  ierr = PetscOptionsGetInt(NULL,NULL,"-data_len_fac",&length,NULL);CHKERRQ(ierr);
   
   switch (test_id) {
       
     case 1:
-      ierr = test_single_write(PETSC_TRUE);CHKERRQ(ierr);
+      ierr = test_single_write(length,PETSC_TRUE);CHKERRQ(ierr);
       break;
       
     case 2:
-      ierr = test_single_read(PETSC_TRUE);CHKERRQ(ierr);
+      ierr = test_single_read(length,PETSC_TRUE);CHKERRQ(ierr);
       break;
 
     case 3:
-      ierr = test_multidata_write(PETSC_FALSE);CHKERRQ(ierr);
+      ierr = test_multidata_write(length,PETSC_FALSE);CHKERRQ(ierr);
       break;
     case 4:
-      ierr = test_multidata_read(PETSC_FALSE);CHKERRQ(ierr);
+      ierr = test_multidata_read(length,PETSC_FALSE);CHKERRQ(ierr);
       break;
 
     default:
