86eeb53f7ca3a2865fbe940b5949472a20fc5f51
Dave May <dave.mayhem23@gmail.com>
2018-03-01 10:07:20 +0000

    DataBucket checkpoint test: * Added quantitive measures to assess success/failure of data being loaded

--- src/tests/test_databucket_checkpoint.c
+++ src/tests/test_databucket_checkpoint.c
@@ -5,6 +5,72 @@
 #include <ptatin_models.h>
 #include <data_bucket.h>
 #include <dmda_checkpoint.h>
+#include <MPntStd_def.h>
+
+/*
+ stats[0,1,2] = min-x,y,z
+ stats[3,4,5] = max-x,y,z
+ stats[6,7,8] = average x,y,z
+*/
+#undef __FUNCT__
+#define __FUNCT__ "MaterialPointComputeCoordStats"
+PetscErrorCode MaterialPointComputeCoordStats(DataBucket db,PetscReal stats[])
+{
+  int d,p,npoints;
+  long int npoints_,npoints_g_;
+  DataField PField_std;
+  double stats_[9],stats_g_[9];
+  PetscErrorCode ierr;
+  
+  for (p=0; p<3; p++) {
+    stats_[p] = 1.0e32;
+  }
+  for (p=3; p<6; p++) {
+    stats_[p] = -1.0e32;
+  }
+  for (p=6; p<9; p++) {
+    stats_[p] = 0.0;
+  }
+  
+  DataBucketGetSizes(db,&npoints,0,0);
+  DataBucketGetDataFieldByName(db,MPntStd_classname,&PField_std);
+  DataFieldGetAccess(PField_std);
+  for (p=0; p<npoints; p++) {
+    MPntStd *material_point;
+    double  *position;
+    
+    DataFieldAccessPoint(PField_std,p,(void**)&material_point);
+    MPntStdGetField_global_coord(material_point,&position);
+    
+    for (d=0; d<3; d++) {
+      stats_[0+d] = PetscMin(stats_[0+d],position[d]);
+    }
+
+    for (d=0; d<3; d++) {
+      stats_[3+d] = PetscMax(stats_[3+d],position[d]);
+    }
+    for (d=0; d<3; d++) {
+      stats_[6+d] += position[d];
+    }
+  }
+  DataFieldRestoreAccess(PField_std);
+
+  ierr = MPI_Allreduce(&stats_[0],&stats_g_[0],3, MPI_DOUBLE, MPI_MIN, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  ierr = MPI_Allreduce(&stats_[3],&stats_g_[3],3, MPI_DOUBLE, MPI_MAX, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  ierr = MPI_Allreduce(&stats_[6],&stats_g_[6],3, MPI_DOUBLE, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
+
+  npoints_ = (long int)npoints;
+  ierr = MPI_Allreduce(&npoints_,&npoints_g_,1, MPI_LONG, MPI_SUM, PETSC_COMM_WORLD);CHKERRQ(ierr);
+  for (d=0; d<3; d++) {
+    stats_g_[6+d] = stats_g_[6+d] / ((double)npoints_g_);
+  }
+  
+  for (d=0; d<9; d++) {
+    stats[d] = (PetscReal)stats_g_[d];
+  }
+  
+  PetscFunctionReturn(0);
+}
 
 #undef __FUNCT__
 #define __FUNCT__ "ptatin_db_checkpoint"
@@ -17,9 +83,10 @@ PetscErrorCode ptatin_db_checkpoint(void)
   Vec            X;
   pTatinModel    model;
   PetscLogDouble time[4];
+  PetscReal      statsOrig[9],statsCP[9];
+  PetscInt       k;
   
   PetscFunctionBegin;
-  
   ierr = PetscOptionsInsertString(NULL,"-output_path cptest");CHKERRQ(ierr);
   
   ierr = pTatin3dCreateContext(&user);CHKERRQ(ierr);
@@ -59,6 +126,8 @@ PetscErrorCode ptatin_db_checkpoint(void)
   ierr = pTatinModel_ApplyInitialMaterialGeometry(model,user);CHKERRQ(ierr);
   
   ierr = pTatinGetMaterialPoints(user,&material_points,NULL);CHKERRQ(ierr);
+  
+  ierr = MaterialPointComputeCoordStats(material_points,statsOrig);CHKERRQ(ierr);
   
   {
     DM dms;
@@ -121,11 +190,33 @@ PetscErrorCode ptatin_db_checkpoint(void)
     }
     */
     
+    ierr = MaterialPointComputeCoordStats(dbload,statsCP);CHKERRQ(ierr);
+
     DataBucketDestroy(&dbload);
   }
   
   PetscPrintf(PETSC_COMM_WORLD,"DataBucket checkpoint write %+1.4e (sec)\n",time[1]-time[0]);
-  PetscPrintf(PETSC_COMM_WORLD,"DataBucket checkpoint load %+1.4e (sec)\n",time[3]-time[2]);
+  PetscPrintf(PETSC_COMM_WORLD,"DataBucket checkpoint load  %+1.4e (sec)\n",time[3]-time[2]);
+  
+  PetscPrintf(PETSC_COMM_WORLD,"  [Orig] x-coor range %+1.12e , %+1.12e\n",statsOrig[0],statsOrig[3]);
+  PetscPrintf(PETSC_COMM_WORLD,"  [Orig] y-coor range %+1.12e , %+1.12e\n",statsOrig[1],statsOrig[4]);
+  PetscPrintf(PETSC_COMM_WORLD,"  [Orig] z-coor range %+1.12e , %+1.12e\n",statsOrig[2],statsOrig[5]);
+  PetscPrintf(PETSC_COMM_WORLD,"  [Orig] coor avg     %+1.12e , %+1.12e , %+1.12e\n",statsOrig[6],statsOrig[7],statsOrig[8]);
+
+  PetscPrintf(PETSC_COMM_WORLD,"  [  CP] x-coor range %+1.12e , %+1.12e\n",statsCP[0],statsCP[3]);
+  PetscPrintf(PETSC_COMM_WORLD,"  [  CP] y-coor range %+1.12e , %+1.12e\n",statsCP[1],statsCP[4]);
+  PetscPrintf(PETSC_COMM_WORLD,"  [  CP] z-coor range %+1.12e , %+1.12e\n",statsCP[2],statsCP[5]);
+  PetscPrintf(PETSC_COMM_WORLD,"  [  CP] coor avg     %+1.12e , %+1.12e , %+1.12e\n",statsCP[6],statsCP[7],statsCP[8]);
+
+  for (k=0; k<9; k++) {
+    PetscReal diff;
+    diff = PetscAbsReal(statsOrig[k]-statsCP[k]);
+    if (diff < 1.0e-8) {
+      PetscPrintf(PETSC_COMM_WORLD,"  coord statistic %D PASSED\n",k);
+    } else {
+      PetscPrintf(PETSC_COMM_WORLD,"  coord statistic %D FAILED\n",k);
+    }
+  }
   
   /* define boundary conditions */
   ierr = pTatinModel_ApplyBoundaryCondition(model,user);CHKERRQ(ierr);
