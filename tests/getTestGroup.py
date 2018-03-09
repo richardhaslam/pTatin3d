03fac8afa6daa9df6ee53550aabb80a308205bb9
Patrick Sanan <patrick.sanan@gmail.com>
2018-03-09 16:59:25 +0900

    Tests: update getTestGroup.py and test makefile targets
    Use proper argument parsing in tests/getTestGroup.py
    
    Remove "classic" tests.

--- tests/getTestGroup.py
+++ tests/getTestGroup.py
@@ -5,50 +5,49 @@
 # Prints a comma-separated list of tests, to be used with pyTestHarness's      #
 # -t option.                                                                   #
 #                                                                              #
-# The first argument can be either a custom group name (e.g. smoke), or a      #
-# subdirectory of $PTATIN_DIR/tests. It is used to choose a subset of tests.   #
-# This may also be the special value "all"                                     #
+# Run with -h for information on options.                                      #
 #                                                                              #
-# Additional arguments can be supplied of the form "no_xxx", which will ignore #
-# any directory with name "xxx". This is useful to skip things like "cuda"     #
-# directories when the library is not configured with CUDA                     #
-#                                                                              #
-# When a subdirectory is specified, looks for files called 'test.py' and       #
+# Looks for files called 'test.py' and                                         #
 # assumes that they define tests named by the relative path to their enclosing #
-# directory, from $PTATIN_DIR.                                                 #
+# directory, from $PTATIN_DIR, with directory separators replaced with dots.   #
 ################################################################################
 
 import os
 import sys
+import argparse
 
-# Custom Groups
-if (len(sys.argv) > 1) :
-    if 'smoke' == sys.argv[1] :
-        print('integration.operator.compare_mf_asm_8x9x11,integration.viscousSolve.asm_8x8x8,application.linear.sinker_galerkin_8x8x8')
-        if (len(sys.argv) > 2) :
-            print('Warning:getTestGroup.py ignoring additional arguments')
-        sys.exit(0)
-    if 'classic' == sys.argv[1] :
-        print('integration.operator.compare_mf_asm_8x9x11,integration.viscousSolve.asm_8x8x8,application.linear.sinker_galerkin_8x8x8,application.linear.sinker_hybrid_8x8x8,application.linear.sinker_hybrid_8x8x8_p8,application.linear.sinker_mf_8x8x8')
-        if (len(sys.argv) > 2) :
-            print('Warning:getTestGroup.py ignoring additional arguments')
-        sys.exit(0)
-    if 'all' == sys.argv[1] :
-        prefix = ''
-    else :
-        prefix = sys.argv[1]
-else :
-    prefix = ''
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--smoke',action='store_true',help='Override other arguments and run smoke tests')
+parser.add_argument('--skip',help='comma-separated list of subdirectories to skip, e.g. --skip cuda,opencl')
+parser.add_argument('--prefix',help='subtree of PTATIN_DIR to restrict to, e.g. -- prefix unit/json')
+args = parser.parse_args()
 
-# If no custom group matched, look for subdirectories of $PTATIN_DIR/tests
+# Custom logic for smoke test
+if (args.smoke) :
+    print('integration.operator.compare_mf_asm_8x9x11,integration.viscousSolve.asm_8x8x8,application.linear.sinker_galerkin_8x8x8')
+    sys.exit(0)
+
+# Look for subdirectories of $PTATIN_DIR/tests
 PTATIN_DIR = os.getenv('PTATIN_DIR')
 if not PTATIN_DIR :
     print("You must define PTATIN_DIR in your environment. Exiting.")
     sys.exit(1)
 
+# Directory names to ignore
+ignore = set(args.skip.split(','))
+if '' in ignore :
+    ignore.remove('')
+
+# Subtree to search in
+if args.prefix :
+    prefix = args.prefix
+else :
+    prefix = ''
+
+# Walk the (sub)tree to create a comma-separated list of tests
 resultString=  ''
 testsDir = os.path.join(PTATIN_DIR,'tests')
-ignore = set([entry[3:] for entry in sys.argv[2:]])            # strip off "no_"
 for (root, dirs, files) in os.walk(os.path.join(testsDir,prefix)) :
     if 'test.py' in files :
         testName = os.path.relpath(root,testsDir).replace(os.sep,'.')
