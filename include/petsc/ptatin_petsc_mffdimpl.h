152c1d4588c3763a92b0dc51891ccbe5660bd434
Dave May <dave.mayhem23@gmail.com>
2018-03-12 09:26:52 +0000

    petsc private headers: add them into the ptatin source tree * This will allow -prefix install of petsc to be used

---
+++ include/petsc/ptatin_petsc_mffdimpl.h
@@ -0,0 +1,14 @@
+#if !defined(__PTATIN_PETSC_MFFD_H__)
+#define __PTATIN_PETSC_MFFD_H__
+
+/* version check */
+#include "petscversion.h"
+
+#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR == 7))
+  #include "src/mat/impls/mffd/mffdimpl.h"
+#else
+  #error "pTatin provided private petsc header for src/mat/impls/mffd/mffdimpl.h is only valid for PETSc v3.7"
+#endif
+
+#endif
+
