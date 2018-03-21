d791dbdbbc49d73fa93fc3f4f94736b6620226fc
Patrick Sanan <patrick.sanan@gmail.com>
2018-03-21 11:54:46 +0100

    All local.mk files: formatting (no literal tabs, 2 space indentation)

--- src/material_constants/local.mk
+++ src/material_constants/local.mk
@@ -1,19 +1,19 @@
 libptatin3d-y.c += $(call thisdir, \
-			MaterialConst_MaterialType_def.c \
-			MaterialConst_DensityConst_def.c \
-			MaterialConst_DensityBoussinesq_def.c \
-			MaterialConst_ViscosityConst_def.c \
-			MaterialConst_ViscosityZ_def.c \
-			MaterialConst_ViscosityArrh_def.c \
-			MaterialConst_ViscosityFK_def.c \
-			MaterialConst_SoftLin_def.c \
-			MaterialConst_SoftExpo_def.c \
-			MaterialConst_PlasticMises_def.c \
-			MaterialConst_PlasticDP_def.c \
-			EnergyMaterialConstants_def.c \
-            EnergySourceConst_def.c \
-			EnergySourceDecay_def.c \
-			EnergySourceAdiabaticAdvection_def.c \
-            EnergyConductivityConst_def.c \
-            EnergyConductivityThreshold_def.c \
-	)
+  MaterialConst_MaterialType_def.c \
+  MaterialConst_DensityConst_def.c \
+  MaterialConst_DensityBoussinesq_def.c \
+  MaterialConst_ViscosityConst_def.c \
+  MaterialConst_ViscosityZ_def.c \
+  MaterialConst_ViscosityArrh_def.c \
+  MaterialConst_ViscosityFK_def.c \
+  MaterialConst_SoftLin_def.c \
+  MaterialConst_SoftExpo_def.c \
+  MaterialConst_PlasticMises_def.c \
+  MaterialConst_PlasticDP_def.c \
+  EnergyMaterialConstants_def.c \
+  EnergySourceConst_def.c \
+  EnergySourceDecay_def.c \
+  EnergySourceAdiabaticAdvection_def.c \
+  EnergyConductivityConst_def.c \
+  EnergyConductivityThreshold_def.c \
+)
