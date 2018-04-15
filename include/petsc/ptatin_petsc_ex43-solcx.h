e8463e772f7a3968ac5af3f4054833f00948bb71
Patrick Sanan <patrick.sanan@gmail.com>
2018-04-15 12:19:13 +0200

    PETSc includes: update to 3.9

--- include/petsc/ptatin_petsc_ex43-solcx.h
+++ include/petsc/ptatin_petsc_ex43-solcx.h
@@ -4,11 +4,10 @@
 /* version check */
 #include "petscversion.h"
 
-#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR == 8))
+#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR == 9))
   #include "src/ksp/ksp/examples/tutorials/ex43-solcx.h"
 #else
-  #error "pTatin provided private petsc header for src/ksp/ksp/examples/tutorials/ex43-solcx.h is only valid for PETSc v3.8"
+  #error "pTatin provided private petsc header for src/ksp/ksp/examples/tutorials/ex43-solcx.h is only valid for PETSc v3.9"
 #endif
 
 #endif
-
--- include/petsc/ptatin_petsc_mffdimpl.h
+++ include/petsc/ptatin_petsc_mffdimpl.h
@@ -4,10 +4,10 @@
 /* version check */
 #include "petscversion.h"
 
-#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR == 8))
+#if ((PETSC_VERSION_MAJOR == 3) && (PETSC_VERSION_MINOR == 9))
   #include "src/mat/impls/mffd/mffdimpl.h"
 #else
-  #error "pTatin provided private petsc header for src/mat/impls/mffd/mffdimpl.h is only valid for PETSc v3.8"
+  #error "pTatin provided private petsc header for src/mat/impls/mffd/mffdimpl.h is only valid for PETSc v3.9"
 #endif
 
 #endif
