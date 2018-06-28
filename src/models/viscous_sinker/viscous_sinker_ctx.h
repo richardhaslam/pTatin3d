af77cb97a8dcee2aea1409fff4ee0298715fcde1
Dave May <dave.mayhem23@gmail.com>
2018-06-28 13:17:44 +0100

    Viscous sinker example: Updated to demonstrate using Dirichlet boundary conditions which change location during a simulation

--- src/models/viscous_sinker/viscous_sinker_ctx.h
+++ src/models/viscous_sinker/viscous_sinker_ctx.h
@@ -31,7 +31,7 @@
 #ifndef __ptatinmodel_viscous_sinker_ctx_h__
 #define __ptatinmodel_viscous_sinker_ctx_h__
 
-typedef enum { VSBC_FreeSlip=0, VSBC_NoSlip, VSBC_FreeSlipFreeSurface, VSBC_NoSlipFreeSurface, VSBC_Test } ViscousSinkerBC;
+typedef enum { VSBC_FreeSlip=0, VSBC_NoSlip, VSBC_FreeSlipFreeSurface, VSBC_NoSlipFreeSurface, VSBC_TimeDependent, VSBC_Test } ViscousSinkerBC;
 
 typedef struct {
   PetscInt  nmaterials;
